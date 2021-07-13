b161c82450353304c5e4734d72aa6232f69b17f5
Md Mahbub SK <skmahbubkr787@gmail.com>
2021-07-13 15:53:16 +0600

    fix: select validation fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const { register, handleSubmit, formState: { errors }, control } = useForm({
+        mode: "onSubmit",
+        reValidateMode: "onSubmit"
+    });
+    const onSubmit = data => console.log(data);
+    // console.log(errors);
+
+    return (
+        <div style={{textAlign:'center'}}>
+            <h2>React Hook Form</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Controller
+                    control={control}
+                    name="gender"
+                    rules={{required: "gender is required"}}
+                    render={({
+                        field: { onChange }
+                    }) => (
+                        <div>
+                            <select onChange={onChange}>
+                                <option value="">select</option>
+                                <option value="male">male</option>
+                                <option value="female">female</option>
+                            </select>
+                            <p>{errors.gender && errors.gender.message}</p>
+                        </div>
+
+                    )}
+                />
+                
+                <button type="submit">submit</button>
+            </form>
+        </div>
+    );
 }
-
-export default App;
